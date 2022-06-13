79bf7e3e09d537ab12b6117d1311654bdcb58d00
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-06-13 19:40:33 +0300

    chore: initialize tailwind and firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
